3e2e69255d571f8385eb609ec87b581a98a5a329
Mikifor <mikifor.astro@gmail.com>
2023-11-05 16:35:35 +0500

    function withRouter goes HOC folder as HOK

--- src/Components/Users/CertainProfile/CertainProfileContainer.jsx
+++ src/Components/Users/CertainProfile/CertainProfileContainer.jsx
@@ -1,21 +1,8 @@
 import CertainProfile from './CertainProfile'
 import { connect } from 'react-redux'
 import React from 'react'
-import { useParams, useLocation, useNavigate } from 'react-router-dom'
 import Preloader from '../Preloader'
-
-function withRouter(Component) {
-    function ComponentWithRouterProp(props) {
-        let location = useLocation();
-        let navigate = useNavigate();
-        let params = useParams();
-        return (
-            <Component {...props} router={{ location, navigate, params }} />
-        )
-    }
-    return ComponentWithRouterProp;
-}
-
+import { withRouter } from '../../../HOC/withRouter'
 
 class CertainProfileAPIComponent extends React.Component {
 
